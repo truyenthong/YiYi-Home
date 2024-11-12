5ec14198ddb221ad31510ce212da341980a44246
du-an-1-lan-2 <truyenthong261997@gmail.com>
2024-11-12 08:21:05 +0700

    update9

--- script-product.js
+++ script-product.js
@@ -1,37 +1,23 @@
-// ====================san pham===========
-const classImg = document.querySelectorAll(
-  ".product-content-left-mall-img img"
-);
-const idImg1 = document.getElementById("product-img-1");
-const idImg2 = document.getElementById("product-img-2");
-const idImg3 = document.getElementById("product-img-3");
-const idImg4 = document.getElementById("product-img-4");
-const idImg5 = document.getElementById("product-img-5");
-const idImg6 = document.getElementById("product-img-6");
-const idImgBig = document.querySelector(".product-big-img");
+const mainImage = document.getElementById("product-big-img");
 
-const spImg = document.getElementById("product-img-1");
+const images = document.querySelectorAll("img");
 
-idImg1.addEventListener("click", () => {
-  idImgBig.src = "./img/kem-nhung/kem-nhung.webp";
+images.forEach((image) => {
+  if (image !== mainImage) {
+    image.addEventListener("click", () => {
+      mainImage.src = image.src;
+      images.forEach((image) => image.classList.remove("active"));
+      image.classList.add("active");
+    });
+  }
 });
+// ========sp=======
+const spImg = document.querySelectorAll(".size img");
+const colorImgs = document.querySelectorAll(" .colorImg");
 
-idImg2.addEventListener("click", () => {
-  idImgBig.src = "./img/kem-nhung/chat-luong-kem.webp";
-});
-
-idImg3.addEventListener("click", () => {
-  idImgBig.src = "./img/kem-nhung/thanh-pham-1.webp";
-});
-
-idImg4.addEventListener("click", () => {
-  idImgBig.src = "./img/kem-nhung/thanh-pham-2.webp";
-});
-
-idImg5.addEventListener("click", () => {
-  idImgBig.src = "./img/kem-nhung/thanh-pham-3.webp";
-});
-
-idImg6.addEventListener("click", () => {
-  idImgBig.src = "./img/kem-nhung/loi-cam-on.webp";
+colorImgs.forEach((colorImg) => {
+  colorImg.addEventListener("click", () => {
+    colorImgs.forEach((colorImg) => colorImg.classList.remove("imgActive"));
+    colorImg.classList.add("imgActive");
+  });
 });
